05ab2723bbc68bb2c0ceee363980eab3ff1197ed
Tiago Leal <tiago.sleal12@gmail.com>
2021-11-07 10:57:49 -0300

    feat: set total in sell model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,7 +1,7 @@
 class Discount < ApplicationRecord
   include Fae::BaseModelConcern
 
-  validates :title, uniqueness: true, presence: true
+  validates :title, presence: true
   validates :value, presence: true
 
   def fae_display_field
--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,19 +1,19 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+  enum status: { Finished: 0, canceled: 1}
+  validates :client, presence: true
 
   belongs_to :discount
   belongs_to :client
-
+  
   has_many :sell_products
   has_many :products, through: :sell_products
 
   has_many :sell_services
   has_many :services, through: :sell_services
   
+  before_save :set_total 
 
-  enum status: { finised: 0, canceled: 1}
-
-  validates :client, presence: true
 
   def fae_display_field
     id
@@ -21,6 +21,21 @@ class Sell < ApplicationRecord
 
   def self.for_fae_index
     order(:id)
+  end
+
+  private
+
+  def set_total
+    total = 0
+    self.products.each {|p| total += p.price }
+    self.services.each {|s| total += s.price }
+
+    if self.discount.present?
+      total = total - self.discount.value
+    end
+
+    total = (total >= 0)? total : 0
+    self.total = total
   end
 
 end
